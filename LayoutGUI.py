fb73e7ee9509c1d8b65093294256fb74993b7c1a
sassystacks <rbeddoes@shaw.ca>
2017-06-15 14:04:03 -0700

    changed layoutGUI

--- LayoutGUI.py
+++ LayoutGUI.py
@@ -4,6 +4,7 @@ import os
 from Connect_Brisco_DB import Connect_DB
 from extractCSV import ExtractCSV
 from datetime import datetime, date, time
+from PIL import Image, ImageTk
 
 def Create_CSV():
     dirPrint =Dir_entry.get()
@@ -31,12 +32,18 @@ def Create_DropDown(root,DD_list):
     root.DD_menu.config(width=8)
     return root.DD_menu
 
+# Main code Start
+
+A = Connect_DB('postgres','postgres','localhost','Boom_boom1')
 root = Tk()
 root.geometry("500x500")
 cwd = os.getcwd()
-image_logo = Tkinter.PhotoImage(file='Brisco_logo.jpg')
+
+img = Image.open("Brisco_logo.png")
+tk_img = ImageTk.PhotoImage(img)
+
 # Label Widgets
-label_image = Label(root,image=photo)
+label_image = Label(root,image=tk_img,borderwidth=2,relief='groove')
 label_year = Label(root,text ="Year")
 label_month = Label(root,text ="Month")
 label_day = Label(root,text ="Day")
@@ -61,20 +68,21 @@ DD_month = Create_DropDown(root,DD_L_month)
 Dir_entry.insert(0,cwd)
 Year_entry.insert(0,"2017")
 
+Dir_entry_row = 1
+Fname_entry_row = 2
+
+
 #Layout Widgets
-label_folder.grid(row=0,column=0,padx=10,sticky=E)
-Dir_entry.grid(row=0,column=1)
-Dir_to_Save.grid(row=0,column=3)
-label_Fname.grid(row=1, column=0,sticky=E)
-Fname_entry.grid(row=1, column=1,padx=10)
-label_month.grid(row=2,column=0,pady=(20,0))
-label_year.grid(row=2,column=1,pady=(20,0))
-Year_entry.grid(row=3,column=1)
-DD_month.grid(row=3, column= 0)
-button_printName.grid(row=4,column=0,sticky=W,pady=30)
-
-
-
-# Day_entry.grid(row=2,column=1)
+label_image.grid(row=0,column=0,sticky=E)
+label_folder.grid(row=1,column=0,sticky=W)
+Dir_entry.grid(row=1,column=0,sticky=E)
+Dir_to_Save.grid(row=1,column=1)
+label_Fname.grid(row=2, column=0,sticky=W)
+Fname_entry.grid(row=2, column=0,sticky=E)
+label_month.grid(row=3,column=0,pady=(20,0))
+label_year.grid(row=3,column=1,pady=(20,0))
+Year_entry.grid(row=4,column=1)
+DD_month.grid(row=4, column= 0)
+button_printName.grid(row=5,column=0,sticky=W,pady=100)
 
 root.mainloop()
